6652c1661cd6df0ca16191a74c67af6904d67713
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-08-06 23:45:07 +0200

    add basic structure sass and header

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../stylesheets/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="div1"></header>
+      <div className="div2"> caja con la frase y la pregunta</div>
+      <div className="div3">
+        input con placeholder para buscar los personajes
+      </div>
+      <div className="div4">cuadrado donde van las tarjetas</div>
+    </div>
+  );
+}
+
+export default App;
