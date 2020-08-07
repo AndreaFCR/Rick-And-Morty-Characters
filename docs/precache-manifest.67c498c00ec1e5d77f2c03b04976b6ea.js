c0526d7d9bba52be799c342964f45024bc06c9c3
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-08-07 13:06:52 +0200

    run docs

---
+++ docs/precache-manifest.67c498c00ec1e5d77f2c03b04976b6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4001ad7c69241c3028e8eb2ec551eedf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13ee1d39d2003af62593",
+    "url": "./static/css/main.030a7fdd.chunk.css"
+  },
+  {
+    "revision": "6db8cab9c0c18a9a7f40",
+    "url": "./static/js/2.444a9a7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.444a9a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ee1d39d2003af62593",
+    "url": "./static/js/main.9bba8cdd.chunk.js"
+  },
+  {
+    "revision": "a9e7643a158b1c4a5932",
+    "url": "./static/js/runtime-main.fa6c3b18.js"
+  }
+]);
