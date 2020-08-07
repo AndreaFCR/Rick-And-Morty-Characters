aac84a921ceff556b453d8f0faabd996f42a7294
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-08-07 19:33:47 +0200

    change funtion data sorted

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,23 +4,23 @@ const getDataFromApi = () => {
   return fetch(ENDPOINT)
     .then((response) => response.json())
     .then((data) => {
-      // sort data by name
-      const dataSorted = () => {
-        return data.results.sort((a, b) => a.name.localeCompare(b.name));
-      };
-
-      // get data attributes I will use
-      return dataSorted().map((character) => {
-        return {
-          episodes: character.episode.length,
-          id: character.id,
-          image: character.image,
-          name: character.name,
-          origin: character.origin.name,
-          specie: character.species,
-          status: character.status,
-        };
-      });
+      return (
+        data.results
+          // sort data by name
+          .sort((a, b) => a.name.localeCompare(b.name))
+          // set data attributes I will use
+          .map((character) => {
+            return {
+              episodes: character.episode.length,
+              id: character.id,
+              image: character.image,
+              name: character.name,
+              origin: character.origin.name,
+              specie: character.species,
+              status: character.status,
+            };
+          })
+      );
     });
 };
 
